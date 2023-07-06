a0533cd9419183916eaab75f1c3df6d9a87ba8fd
Zhang Zhao <111113097+cookieiz@users.noreply.github.com>
2023-07-06 09:29:19 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 export function onLoad() {
 
-  const element = document.querySelector("#app > div.container > aside > div.sidebar__upper > div.nav.sidebar__nav > div:nth-child(4)");
+  const element = document.querySelector("#app > div.container > aside > div.sidebar__upper > div.nav.sidebar__nav > div:nth-child(3)");
   if (element) {
     element.parentNode.removeChild(element);
   }
